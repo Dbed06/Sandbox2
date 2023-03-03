47a14b4b0a224f9dc88e87217832672e99ce0356
Dbed06 <deebed69@icloud.com>
2023-03-03 01:41:38 -0700

    bigbox

---
+++ BigBox/src/main/java/org/example/comparable/Orange.java
@@ -0,0 +1,60 @@
+package org.example.comparable;
+
+import java.util.Objects;
+
+public class Orange implements Comparable<Orange>{
+
+    private Size size;
+
+    public Orange() {
+    }
+
+    public Orange(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Orange orange = (Orange) o;
+        return size == orange.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(size);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Orange{");
+        sb.append("size=").append(size);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public int compareTo(Orange o) {
+
+        if(this.size.ordinal()< o.size.ordinal()) {
+            return -1;
+        }
+        else if (this.size.ordinal() > o.size.ordinal()) {
+            return 1;
+
+        }
+        else {
+            return 0;
+        }
+    }
+}
+
---
+++ BigBox/src/main/java/org/example/comparable/Size.java
@@ -0,0 +1,8 @@
+package org.example.comparable;
+
+public enum Size {
+
+    SMALL, MEDIUM, LARGE, XLARGE
+
+}
+

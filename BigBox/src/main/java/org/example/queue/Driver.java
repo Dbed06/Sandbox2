449fe6f1788ef77cfd72eda68d633dde4b24afb4
Dbed06 <deebed69@icloud.com>
2023-03-27 23:36:25 -0700

    bigbox

---
+++ BigBox/src/main/java/org/example/queue/Driver.java
@@ -0,0 +1,19 @@
+
+    package org.example.queue;
+
+    public class Driver {
+
+        public static void main(String[] args) {
+
+            Queue<String> queue = new ArrayListQueue<>();
+
+            queue.offer("One");
+            queue.offer("Two");
+            queue.offer("Three");
+            queue.offer("Four");
+
+            System.out.println(queue);
+        }
+    }
+
+
---
+++ BigBox/src/main/java/org/example/shuntingyard/Driver.java
@@ -0,0 +1,11 @@
+
+
+package org.example.shuntingyard;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        System.out.println(ShuntingYard.postFix("3+4*5"));
+    }
+}
